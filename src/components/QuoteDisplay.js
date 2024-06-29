69295990792c711261be7f493652d4f059ddd295
sahil-sangtani <sahilsangtani01@gmail.com>
2024-06-29 22:01:40 +0530

    done

--- src/components/QuoteDisplay.js
+++ src/components/QuoteDisplay.js
@@ -1,19 +1,3 @@
-// import React from "react";
-// import { Typography, Paper } from "@mui/material";
-
-// const QuoteDisplay = ({ quote }) => {
-//   return (
-//     <Paper elevation={3} sx={{ p: 2, mb: 2 }}>
-//       <Typography variant="body1">"{quote.text}"</Typography>
-//       <Typography variant="subtitle2" align="right">
-//         - {quote.author}
-//       </Typography>
-//     </Paper>
-//   );
-// };
-
-// export default QuoteDisplay;
-
 
 import React from "react";
 import { Typography, Box } from "@mui/material";
